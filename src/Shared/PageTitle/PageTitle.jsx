d6a9b21eeb825685a0321c1311718cb3559aba6d
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-13 02:23:01 +0600

    go to dashbord routs chnage

--- src/Shared/PageTitle/PageTitle.jsx
+++ src/Shared/PageTitle/PageTitle.jsx
@@ -6,7 +6,7 @@ import { FaArrowLeft } from "react-icons/fa";
 const PageTitle = ({title, mainPage, page}) => {
     return (
         <div className="flex items-center justify-between bg-primaryColor py-2 px-1 lg:px-6 text-white">
-            <h5 className="text-2xl"><Link className="flex items-center gap-2" to='/'><FaArrowLeft></FaArrowLeft>Back To Dashbord</Link></h5>
+            <h5 className="text-2xl"><Link className="flex items-center gap-2" to='/dashbordgit /'><FaArrowLeft></FaArrowLeft>Back To Dashbord</Link></h5>
             <div className="flex items-center gap-2">
                 <Link to=''><IoHomeOutline className="text-xl"></IoHomeOutline></Link> / <span>{mainPage}</span> / <span>{page}</span>
             </div>
