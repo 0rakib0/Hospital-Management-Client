0083ceddf7dd1111a4638a3ff710913f09171fa4
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-14 02:11:30 +0600

    customize aout us title text

--- src/Pages/Home/AboutUs.jsx
+++ src/Pages/Home/AboutUs.jsx
@@ -10,9 +10,9 @@ const AboutUs = () =>{
                     <img src="https://aquinahealth.com/wp-content/uploads/2018/09/Pulse_91918.jpeg" alt="" />
                 </div>
                 <div className="lg:w-6/12 lg:mt-12">
-                    <p className="text-primaryColor font-semibold">ABOUT US</p>
+                    <p className="font-semibold"><span className="text-primaryColor">ABOUT</span> US</p>
                     <h4 className="text-4xl py-2 font-semibold">Something about this hospital and about us</h4>
-                    <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis explicabo modi nulla qui neque, adipisci laboriosam aspernatur. Debitis perferendis doloremque ad minus vero molestias optio aliquid in, non eum animi quo, dolorum facere sed earum?</p>
+                    <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis explicabo modi nulla qui neque, adipisci laboriosam aspernatur. Debitis perferendis doloremque ad minus vero molestias optio aliquid in, non eum animi quo, dolorum facere sed earum? optio aliquid in, non eum animi quo, dolorum facere sed earum?optio aliquid in, non eum animi quo, dolorum facere sed earum?</p>
                     <button className="button py-2 px-4 flex items-center gap-2 hover:gap-4">Now More <FaArrowRight className="icon"></FaArrowRight></button>
                 </div>
             </div>
