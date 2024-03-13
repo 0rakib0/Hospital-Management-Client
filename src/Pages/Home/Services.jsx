c5398ba0a37a0d518c347fd7413b9a20c4aac326
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-14 01:20:12 +0600

    apply hover effect in service card

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -19,49 +19,49 @@ const Service = () => {
                     <h4 className="text-2xl font-semibold">Emergency Care</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corrupti soluta quibusdam explicabo maiores. Ipsam praesentium id nisi.</p>
                 </div>
-                <div className="bg-white p-4 text-center flex flex-col items-center">
+                <div className="bg-white p-4 text-center flex flex-col items-center hover:border hover:border-primaryColor duration-300 hover:rounded-lg">
                     <div className="text-6xl text-primaryColor mb-2">
                         <HiClipboardDocumentList></HiClipboardDocumentList>
                     </div>
                     <h4 className="text-2xl font-semibold">Free Checkoup</h4>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi laudantium incidunt minus praesentium, eos quis quisquam porro consectetur.</p>
                 </div>
-                <div className="bg-white p-4 text-center flex flex-col items-center">
+                <div className="bg-white p-4 text-center flex flex-col items-center hover:border hover:border-primaryColor duration-300 hover:rounded-lg">
                     <div className="text-6xl text-primaryColor mb-2">
                         <FaBedPulse></FaBedPulse>
                     </div>
                     <h4 className="text-2xl font-semibold">Oparattion & Sergary</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius facilis libero quibusdam enim a, nisi repellendus ut cumque nihil, atque, sequi natus amet provident?</p>
                 </div>
-                <div className="bg-white p-4 text-center flex flex-col items-center">
+                <div className="bg-white p-4 text-center flex flex-col items-center hover:border hover:border-primaryColor duration-300 hover:rounded-lg">
                     <div className="text-6xl text-primaryColor mb-2">
                         <FaUserDoctor></FaUserDoctor>
                     </div>
                     <h4 className="text-2xl font-semibold">Expart Doctor</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius facilis libero quibusdam enim a, nisi repellendus ut cumque nihil, atque, sequi natus amet provident?</p>
                 </div>
-                <div className="bg-white p-4 text-center flex flex-col items-center">
+                <div className="bg-white p-4 text-center flex flex-col items-center hover:border hover:border-primaryColor duration-300 hover:rounded-lg">
                     <div className="text-6xl text-primaryColor mb-2">
                         <FaAmbulance></FaAmbulance>
                     </div>
                     <h4 className="text-2xl font-semibold">24/7 Ambulance</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius facilis libero quibusdam enim a, nisi repellendus ut cumque nihil, atque, sequi natus amet provident?</p>
                 </div>
-                <div className="bg-white p-4 text-center flex flex-col items-center">
+                <div className="bg-white p-4 text-center flex flex-col items-center hover:border hover:border-primaryColor duration-300 hover:rounded-lg">
                     <div className="text-6xl text-primaryColor mb-2">
                         <BiSolidDonateBlood></BiSolidDonateBlood>
                     </div>
                     <h4 className="text-2xl font-semibold">Free Blood Testiong</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius facilis libero quibusdam enim a, nisi repellendus ut cumque nihil, atque, sequi natus amet provident?</p>
                 </div>
-                <div className="bg-white p-4 text-center flex flex-col items-center">
+                <div className="bg-white p-4 text-center flex flex-col items-center hover:border hover:border-primaryColor duration-300 hover:rounded-lg">
                     <div className="text-6xl text-primaryColor mb-2">
                         <TbMedicineSyrup></TbMedicineSyrup>
                     </div>
                     <h4 className="text-2xl font-semibold">Medicine & Phermacy</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius facilis libero quibusdam enim a, nisi repellendus ut cumque nihil, atque, sequi natus amet provident?</p>
                 </div>
-                <div className="bg-white p-4 text-center flex flex-col items-center">
+                <div className="bg-white p-4 text-center flex flex-col items-center hover:border hover:border-primaryColor duration-300 hover:rounded-lg">
                     <div className="text-6xl text-primaryColor mb-2">
                         <GrDocumentConfig></GrDocumentConfig>
                     </div>
