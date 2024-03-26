57ad7f292b77868885bb6ee28e2f63cce80f8afc
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-27 03:38:58 +0600

    add skaliton in patients dashbord

--- src/Pages/Dashbord/PatientsDashbord.jsx
+++ src/Pages/Dashbord/PatientsDashbord.jsx
@@ -8,145 +8,166 @@ import useNoDataMessage from "../../Hook/useNoDataMessage";
 const PatientsDashbord = () => {
 
     const asiouxSecure = useAxious()
-    const { user, userInfo } = useAuth()
+    const { user } = useAuth()
+    const [loading, setLoading] = useState(false)
+    const [payloading, setPayLoading] = useState(false)
+    const [appoinmentloading, setAppoinmentLoading] = useState(false)
 
-    const userType = userInfo?.user_type
     const [patient, setPatients] = useState([])
     const [payments, setPayment] = useState([])
     const [PatientAppoinment, setAppoinment] = useState([])
 
 
-    
+
 
     const patientsId = patient?.id
     useEffect(() => {
-        
+        setLoading(true)
         asiouxSecure.get(`patients/single-patient/${user}/`)
-        .then(res => {
-            setPatients(res.data)
-        })
-        
+            .then(res => {
+                setPatients(res.data)
+                setLoading(false)
+            })
+
     }, [user])
-    
+
     console.log(patient)
     useEffect(() => {
+        setPayLoading(true)
         if (patientsId) {
-        asiouxSecure.get(`/payments/${patientsId}`)
-            .then(res => {
-                setPayment(res.data)
-            })
-        }
-
-    }, [patientsId])
-
-
-    useEffect(() => {
-        if (patientsId) {
-            asiouxSecure.get(`/patients-appoinment/${patientsId}`)
+            asiouxSecure.get(`/payments/${patientsId}`)
                 .then(res => {
-                    setAppoinment(res.data)
+                    setPayment(res.data)
+                    setPayLoading(false)
+                })
+                .catch(error => {
+                    alert(error)
                 })
         }
 
     }, [patientsId])
+
+
+    useEffect(() => {
+        setAppoinmentLoading(true)
+        if (patientsId) {
+            asiouxSecure.get(`/patients-appoinment/${patientsId}`)
+                .then(res => {
+                    setAppoinment(res.data)
+                    setAppoinmentLoading(false)
+                })
+                .catch(error => {
+                    alert(error)
+                })
+        }
+
+    }, [patientsId])
+
+
 
     return (
         <>
             <div className="my-4 p-4 rounded-md shadow-lg">
                 <div className="flex lg:flex-row flex-col gap-4">
                     <div className="lg:w-6/12 bg-white">
-                        <p className="text-2xl pl-4 pt-2 font-semibold text-primaryColor">Patient Details</p>
-                        <div className="bg-primaryColor w-[15rem] h-[15rem] overflow-hidden ml-6 mt-4">
-                            <img src={patient?.patients_pic} className="w-full" alt="" />
-                        </div>
-                        <div className="overflow-x-auto my-4 ">
-                            <table className="table">
-                                <tbody>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Patient ID
-                                        </td>
-                                        <td className="border">
-                                            {patient?.patients_Id}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Full Name
-                                        </td>
-                                        <td className="border">
-                                            {patient?.full_name}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Date Of Birth
-                                        </td>
-                                        <td className="border">
-                                            {patient?.date_of_birth}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Age
-                                        </td>
-                                        <td className="border">
-                                            {patient?.age}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Phone
-                                        </td>
-                                        <td className="border">
-                                            {patient?.phone}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Email
-                                        </td>
-                                        <td className="border">
-                                            {patient?.email}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Gender
-                                        </td>
-                                        <td className="border">
-                                            {patient?.gender}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Full Address
-                                        </td>
-                                        <td className="border">
-                                            {patient?.full_address}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Details About Patients
-                                        </td>
-                                        <td className="border">
-                                            {patient?.details}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border font-bold">
-                                            Patients Status
-                                        </td>
-                                        <td className="border">
-                                            {patient?.disCharged ? <p>Dischared</p> :
-                                                <p>In Treatment</p>
-                                            }
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        {loading ? <div className="flex flex-col gap-4 w-52 mt-12 ml-12 pb-6">
+                            <div className="skeleton h-32 w-full"></div>
+                            <div className="skeleton h-4 w-28"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                        </div> : <> <p className="text-2xl pl-4 pt-2 font-semibold text-primaryColor">Patient Details</p>
+                            <div className="bg-primaryColor w-[15rem] h-[15rem] overflow-hidden ml-6 mt-4">
+                                <img src={patient?.patients_pic} className="w-full" alt="" />
+                            </div>
+                            <div className="overflow-x-auto my-4 ">
+                                <table className="table">
+                                    <tbody>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Patient ID
+                                            </td>
+                                            <td className="border">
+                                                {patient?.patients_Id}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Full Name
+                                            </td>
+                                            <td className="border">
+                                                {patient?.full_name}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Date Of Birth
+                                            </td>
+                                            <td className="border">
+                                                {patient?.date_of_birth}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Age
+                                            </td>
+                                            <td className="border">
+                                                {patient?.age}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Phone
+                                            </td>
+                                            <td className="border">
+                                                {patient?.phone}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Email
+                                            </td>
+                                            <td className="border">
+                                                {patient?.email}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Gender
+                                            </td>
+                                            <td className="border">
+                                                {patient?.gender}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Full Address
+                                            </td>
+                                            <td className="border">
+                                                {patient?.full_address}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Details About Patients
+                                            </td>
+                                            <td className="border">
+                                                {patient?.details}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border font-bold">
+                                                Patients Status
+                                            </td>
+                                            <td className="border">
+                                                {patient?.disCharged ? <p>Dischared</p> :
+                                                    <p>In Treatment</p>
+                                                }
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div> </>}
+
                     </div>
                     <div className="bg-white lg:w-6/12 p-2 overflow-x-scroll">
                         <p className="text-2xl pl-4 pt-2 font-semibold text-primaryColor">Appoinmet Details</p>
@@ -196,6 +217,12 @@ const PatientsDashbord = () => {
 
                             </tbody>
                         </table>
+                        {appoinmentloading && <div className="flex flex-col gap-4 w-full mt-6">
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-tull"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                        </div>}
                         <p className="text-center my-6">{useNoDataMessage(PatientAppoinment)}</p>
                     </div>
                 </div>
@@ -248,8 +275,13 @@ const PatientsDashbord = () => {
                             }
 
                         </tbody>
-
                     </table>
+                    {payloading && <div className="flex flex-col gap-4 w-full mt-6">
+                        <div className="skeleton h-4 w-full"></div>
+                        <div className="skeleton h-4 w-tull"></div>
+                        <div className="skeleton h-4 w-full"></div>
+                        <div className="skeleton h-4 w-full"></div>
+                    </div>}
                     <p className="text-center my-6">{useNoDataMessage(payments)}</p>
                 </div>
             </div>
